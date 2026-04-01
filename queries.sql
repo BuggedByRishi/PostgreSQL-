826bef67ed30ddf2fa4bea1307ef234059f08535
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-04-01 16:48:24 +0530

    Querry to fetch records by ID

--- queries.sql
+++ queries.sql
@@ -1,7 +1,1 @@
-INSERT INTO majdoor (name, department, salary)
-VALUES
-('Suresh Patil', 'Electrician', 30000.00),
-('Amit Sharma', 'Plumbing', 28000.75),
-('Rahul Verma', 'Construction', 26000.00),
-('Vikram Singh', 'Carpentry', 32000.25),
-('Manoj Yadav', 'Electrician', 29000.50);
+select * FROM majdoor WHERE id = 1;
