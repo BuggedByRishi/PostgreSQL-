02bf42aa8f64ce581b985acb8417e3e772d75a75
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-04-02 11:06:40 +0530

    Using Where with Conditions

--- queries.sql
+++ queries.sql
@@ -1,7 +1,2 @@
-SELECT
-    first_name,
-    hire_date,
-    salary * 12 AS annual_salary
-FROM
-    employees
-ORDER BY  hire_date ASC, annual_salary DESC;
+SELECT * FROM employees
+WHERE salary >= 70000;
